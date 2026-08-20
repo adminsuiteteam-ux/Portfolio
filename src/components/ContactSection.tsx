86a333ba8fee9e5035ac5cba4efee783bf5b55c5
Brownforte Mechanical <adminsuiteteam-ux@github.com>
2026-08-20 14:36:38 -0800

    fix: suppress i18n lint warnings in portfolio components

--- src/components/ContactSection.tsx
+++ src/components/ContactSection.tsx
@@ -1,7 +1,8 @@
-/* eslint-disable */
+/* eslint-disable @shopify/jsx-no-hardcoded-content */
 import React, { useState } from 'react';
 import { FadeIn } from './FadeIn';
 import { Mail, Phone, MapPin, Send, CheckCircle } from 'lucide-react';
+import { ContactButton } from './ContactButton';
 
 export const ContactSection: React.FC = () => {
   const [submitted, setSubmitted] = useState(false);
--- src/components/PricingSection.tsx
+++ src/components/PricingSection.tsx
@@ -1,4 +1,4 @@
-/* eslint-disable */
+/* eslint-disable @shopify/jsx-no-hardcoded-content */
 import React from 'react';
 import { FadeIn } from './FadeIn';
 import { CheckCircle2, MessageSquare, Info } from 'lucide-react';
